f422f381b44c233b5731410447488f7877911a9e
Ben Low <7970559+af3556@users.noreply.github.com>
2025-04-23 21:05:32 +1000

    simplify output switching

--- snippets/pingshelly.sh
+++ snippets/pingshelly.sh
@@ -16,16 +16,26 @@ curl_opts() {
 MAXTEMP=60
 NTFY=https://ntfy.sh/jonsson-pumphouse-loadshed
 
-if (( $# < 1 )); then
+if (( $#< 1 )); then
   echo "usage: $0 shellyhost" >&2
   exit 1
 fi
 
 SHELLY="$1"
 STATE_FILE=/var/local/$(basename "$0").$(basename "$SHELLY" .).state
-LOG_FILE=${STATE_FILE%.state}.log
 
-# write the uptime and errcount variables to a 'state' file on our way out
+# when connected to a tty send output to stdout; otherwise (e.g. via cron)
+# append to a log file
+# test -t n: true if fd n is connected to a tty
+# in days of yore you had to pick an fd and hope it wasn't being used
+# (or more rigourously search for a free fd); as of 4.1 bash will allocate one
+# for you (and store the allocated fd in LOG_FD, here)
+if [ -t 1 ]; then
+  exec {LOG_FD}>&1
+else
+  exec {LOG_FD}>>"${STATE_FILE%.state}.log"  # append
+fi
+
 trap 'declare -p uptime errcount > "$STATE_FILE"' EXIT
 
 uptime=0
@@ -73,12 +83,7 @@ uptime=$(curl_opts "http://${SHELLY}/rpc/Shelly.GetStatus" |
     fi
     # %T: -1 = current time
     printf -v logmsg "%(%F %R)T\t%s\t%s" -1 "$newuptime" "$temperature"
-    # echo log to stderr when it's connected to a tty (i.e. not cron)
-    if [ -t 2 ]; then
-      echo "$logmsg" | tee --append "$LOG_FILE" >&2
-    else
-      echo "$logmsg" >> "$LOG_FILE"
-    fi
+    echo "$logmsg" >&${LOG_FD}
     # return new uptime to be written to the state file (we're in a subshell)
     echo "$newuptime"
   })
